4e38ecb1350e3d9580c1fd9412a641fabdc604b3
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-20 09:23:45 -0400

    CORE COMPLETE

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w( Fiction Non-Fiction )}
+
+    # validate :title, :sufficient_clickbait
+
+    # def sufficient_clickbait
+
+    # end
 end
